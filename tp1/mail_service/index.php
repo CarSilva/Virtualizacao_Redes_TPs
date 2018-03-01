43f72e954aaeff51ede1fb6c0938e564fffde40d
Pedro Cunha <pedrocunha1596@gmail.com>
2018-03-01 11:12:35 +0000

    base de serviço de email a funcionar

--- tp1/mail_service/index.php
+++ tp1/mail_service/index.php
@@ -9,21 +9,21 @@
 <?php
 require 'PHPMailerAutoload.php';
 
-$mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
+$mail = new PHPMailer;                              // Passing `true` enables exceptions
+//try {
     //Server settings
     $mail->SMTPDebug = 2;                                 // Enable verbose debug output
     $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'mail.mysmtp.com';                      // Specify main and backup SMTP servers
-    $mail->SMTPAuth = false;                               // Enable SMTP authentication
-    $mail->Username = 'g1';                               // SMTP username
-    $mail->Password = 'g1';                               // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail->Host = 'mail';                          // Specify main and backup SMTP servers
+    //$mail->SMTPAuth = false;                              // Enable SMTP authentication
+    //$mail->Username = 'g1';                               // SMTP username
+    //$mail->Password = 'g1';                               // SMTP password
+    //$mail->SMTPSecure = 'tls';                          // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 25;                                     // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('g1@example.com');
-    $mail->addAddress('pedrocunha1596@gmail.com');        // Add a recipient
+    $mail->setFrom('g1@g1.gcom.di.uminho.pt');
+    $mail->addAddress('pedronyk1996@hotmail.com');        // Add a recipient
     //$mail->addReplyTo('info@example.com', 'Information');
     //$mail->addCC('cc@example.com');
     //$mail->addBCC('bcc@example.com');
@@ -40,7 +40,7 @@ try {
 
     $mail->send();
     echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
+//} catch (Exception $e) {
+  //  echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+//}
 ?>
