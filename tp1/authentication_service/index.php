0995795344a8f67903c0154f969f5970efb6385c
Pedro Cunha <pedrocunha1596@gmail.com>
2018-03-03 19:32:07 +0000

    conexao entre auth e mail

--- tp1/authentication_service/index.php
+++ tp1/authentication_service/index.php
@@ -12,60 +12,9 @@
 <html lang = "pt">
    
    <head>
+
+      <link rel="stylesheet" type="text/css" href="style.css">
       <title>Serviço de Autenticação</title>
-      
-      <style>
-         body {
-            font-family: Arial, Helvetica, sans-serif;
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #ADABAB;
-         }
-         
-         
-         input[type=text], select {
-             width: 25%;
-             padding: 12px;
-             border: 1px solid #ccc;
-             border-radius: 4px;
-             box-sizing: border-box;
-             margin-top: 6px;
-             margin-bottom: 16px;
-             resize: vertical;
-         }
-         textarea{
-             width: 100%;
-             padding: 12px;
-             border: 1px solid #ccc;
-             border-radius: 4px;
-             box-sizing: border-box;
-             margin-top: 6px;
-             margin-bottom: 16px;
-             resize: vertical;  
-         }
-
-         input[type=submit] {
-             background-color: #4CAF50;
-             color: white;
-             padding: 12px 20px;
-             border: none;
-             border-radius: 4px;
-             cursor: pointer;
-         }
-
-         input[type=submit]:hover {
-             background-color: #45a049;
-         }
-
-         h1 {
-           text-align: center;
-         }
-         .container {
-             border-radius: 5px;
-             background-color: #f2f2f2;
-             padding: 20px;
-         }
-      </style>
    </head>
    
    <body>
@@ -75,6 +24,13 @@
             $msg = '';
             $token = '';
             $login;
+            $dbuser = postgres;
+            $dbpass = postgres;
+            try {
+              $conn = new PDO("pgsql:host=db;port=5432;dbname=auth", $dbuser, $dbpass);
+            } catch (PDOException $e) {
+              die('Connection failed: ' . $e->getMessage());
+            }
             if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
                if (!isset($login[$_POST['username']])){
                   $login[$_POST['username']] = $_POST['password'];
@@ -113,19 +69,7 @@
          <form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
            ?>" method = "post">
             <label for="subject">Token:</label>
-            <textarea id="subject" name="mensagem" placeholder="Token..." style="height:35px"><?php echo $token; ?></textarea>
-
-         <!--<form class = "form-signin" role = "form" 
-            action = "<?php// echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h2><?php //print $msg; ?></h2>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password" required>
-            <input class = "play_button" type = "submit" value = "Login" name = "login">
-         -->   
+            <textarea id="subject" name="mensagem" placeholder="Token..." style="height:35px"><?php echo $token; ?></textarea>  
          </form>  
       </div> 
       
