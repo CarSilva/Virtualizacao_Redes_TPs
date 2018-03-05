69cf946a7f133345991c67377e675249ab7c190f
Carlos Silva <carlosmms96@gmail.com>
2018-03-05 14:05:56 +0000

    Corrigido problema de falta de include no database.php

---
+++ tp1/authentication_service/database.php
@@ -0,0 +1,47 @@
+<?php
+function get_connection(){
+    $dbuser = postgres;
+    $conn;
+    try {
+      $conn = new PDO("pgsql:host=db;dbname=postgres", $dbuser, $dbuser);
+    } catch (PDOException $e) {
+      die('Connection failed: ' . $e->getMessage());
+    }
+    return $conn;
+  }
+  function  insert_into_table($conn, $token, $name, $pass){
+    $conn->beginTransaction();
+    $result = $conn->exec("INSERT INTO tokens VALUES ('$token', '$name', '$pass');");
+    $conn->commit();
+  }
+  function select_table($conn){
+    $sql = 'SELECT * FROM tokens;';
+    $table = $conn->query($sql);
+    return $table;
+  }
+  function exists_name($table, $name, $pass){
+    $result = -2;
+    foreach ($table as $row) {
+      if($row["name"] == $name && $row["password"] == $pass){
+        $result = $row["token"];
+        break;
+      }
+      else if($row["name"] == $name && $row["password"] != $pass){
+        $result = -1;
+        break;
+      }
+    }
+    return $result;
+  }
+
+  function exists_token($table, $token){
+    $result = 0;
+    foreach ($table as $row) {
+      if($row["token"] == $token){
+        $result = 1;
+        break;
+       }
+     }
+    return $result;
+  }
+?>
--- tp1/authentication_service/server.php
+++ tp1/authentication_service/server.php
@@ -1,42 +1,15 @@
 <?php
-	function get_connection(){
-		$dbuser = postgres;
-		$conn;
-		try {
-			$conn = new PDO("pgsql:host=db;dbname=postgres", $dbuser, $dbuser);
-		} catch (PDOException $e) {
-			die('Connection failed: ' . $e->getMessage());
-		}
-		return $conn;
+	include "database.php";
+	header("Content-Type:application/json");
+	$data = json_decode(file_get_contents('php://input'), true);
+	$token = $data['token'];
+	$conn = get_connection();
+	$table = select_table($conn);
+	$exists = exists_token($table, $token);
+	if($exists == 1){
+		print_r('1');
 	}
-
-	function select_table($conn){
-		$sql = 'SELECT * FROM tokens;';
-		$table = $conn->query($sql);
-		return $table;
+	else{
+		print_r('0');
 	}
-
-	function exists_token($table, $token){
-		$result = 0;
-	  foreach ($table as $row) {
-	    if($row["token"] == $token){
-	      $result = 1;
-	      break;
-	     }
-	   }
-	   return $result;
-	}
-
-		header("Content-Type:application/json");
-		$data = json_decode(file_get_contents('php://input'), true);
-		$token = $data['token'];
-		$conn = get_connection();
-		$table = select_table($conn);
-		$exists = exists_token($table, $token);
-		if($exists == 1){
-			print_r('1');
-		}
-		else{
-			print_r('0');
-		}
 ?>
