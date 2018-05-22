6df97492e80dfab357ef257f5d02e3f82c96bcef
Carlos Silva <carlosmms96@gmail.com>
2018-05-22 15:17:12 +0100

    Servidor de ficheiros implementado. Com download, listagem e upload

---
+++ tp3/Client.java
@@ -0,0 +1,148 @@
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.net.*;
+import java.util.Scanner;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Client {
+
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        try {
+            byte[] buf;
+            DatagramPacket recv;
+            DatagramSocket server = new DatagramSocket(9999);
+            String recv_str;
+            String cleaned;
+            StringBuilder sb = new StringBuilder();
+            System.out.println(initMenu());
+            while (true) {
+                String next = scan.next();
+                switch (next) {
+
+                    case "1":
+                        forwardingUDP(InetAddress.getByName("127.0.0.1"), next);
+                        buf = new byte[1024];
+                        recv = new DatagramPacket(buf, buf.length);
+                        server.receive(recv);
+                        recv_str = new String(recv.getData());
+                        cleaned = recv_str.substring(0, cmp(recv_str));
+                        System.out.println(cleaned);
+                        System.out.println(initMenu());
+                        break;
+
+                    case "2":
+                        System.out.println("Escolha o ficheiro a fazer Download");
+                        next = scan.next();
+                        String send = "2&" + next;
+                        forwardingUDP(InetAddress.getByName("10.0.0.250"), send);
+                        buf = new byte[1024];
+                        recv = new DatagramPacket(buf, buf.length);
+                        server.receive(recv);
+                        recv_str = new String(recv.getData());
+                        cleaned = recv_str.substring(0, cmp(recv_str));
+                        while(true){
+                            if(cleaned.contains("END")){
+                                System.out.println(initMenu());
+                                break;
+                            }
+                            sb.append(cleaned);
+                            buf = new byte[1024];
+                            recv = new DatagramPacket(buf, buf.length);
+                            server.receive(recv);
+                            recv_str = new String(recv.getData());
+                            cleaned = recv_str.substring(0, cmp(recv_str));
+                        }
+                        writeFile(next, sb.toString());
+                        sb = new StringBuilder();
+                        break;
+
+                    case "3":
+                        System.out.println("Escolha o ficheiro a fazer Upload");
+                        next = scan.next();
+                        List<String> content = readFile(next);
+                        sb.append("3").append("&").append(next).append("&");
+                        for(String b : content){
+                            sb.append(b).append("\n");
+                        }
+                        forwardingUDP(InetAddress.getByName("10.0.0.250"), sb.toString());
+                        sb = new StringBuilder();
+                        System.out.println(initMenu());
+                        break;
+
+                    default:
+                        System.out.println("NOT SUPPORTED OPTION");
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static List<String> readFile(String name){
+        List<String> file = new ArrayList<>();
+        Path path = Paths.get("files/" + name);
+        try {
+            file = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    public static void writeFile(String fileName, String content) {
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(content);
+            writer.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void forwardingUDP(InetAddress dest, String msg) {
+
+        try {
+            byte[] sendData = new byte[1024];
+            DatagramSocket clientUDP = new DatagramSocket();
+            sendData = msg.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, dest, 11111);
+            clientUDP.send(sendPacket);
+            clientUDP.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public static String initMenu(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("1.Listar Ficheiros\n");
+        sb.append("2.Efetuar Download\n");
+        sb.append("3.Efetuar Upload\n");
+        return sb.toString();
+
+    }
+
+    private static int cmp (String a){
+        int i=0;
+        while(i < a.length()){
+            char l = a.charAt(i);
+            int ascci = (int) l;
+            if(l == 0){
+                break;
+            }
+            else{
+                i++;
+            }
+        }
+        return i;
+    }
+}
---
+++ tp3/Server.java
@@ -0,0 +1,133 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.net.InetAddress;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+
+    public static void main(String [] args) {
+        byte[] buf;
+        try {
+            DatagramSocket server = new DatagramSocket(11111);
+            while(true){
+                buf = new byte[1024];
+                DatagramPacket recv = new DatagramPacket(buf, buf.length);
+                server.receive(recv);
+                String recv_msg = new String(recv.getData());
+                String cleaned = recv_msg.substring(0, cmp(recv_msg));
+                String [] a = cleaned.split("&");
+                InetAddress dest = recv.getAddress();
+                switch(a[0]){
+                    case "1":
+                        String ss = getFiles();
+                        forwardingUDP(dest, ss);
+                        break;
+                    case "2":
+                        List<String> file = readFile(a[1]);
+                        file.add("END");
+                        for (String s : file){
+                            forwardingUDP(dest, s);
+                        }
+                        break;
+                    case "3":
+                        String name = a[1];
+                        String fileContent = a[2];
+                        writeFile(name, fileContent);
+                        break;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String filesMenu(){
+        StringBuilder sb = new StringBuilder();
+
+        return sb.toString();
+    }
+
+    public static String getFiles(){
+        Process p;
+        StringBuilder sb = new StringBuilder();
+        String line = "";
+        int counter = 0;
+        try{
+            p = Runtime.getRuntime().exec("ls files");
+            p.waitFor();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while((line = reader.readLine()) != null) {
+                sb.append(counter + ": " + line + "\n");
+                counter ++;
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return sb.toString();
+
+    }
+
+    public static void writeFile(String fileName, String content) {
+        try{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            writer.write(content);
+            writer.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static List<String> readFile(String name){
+        List<String> file = new ArrayList<>();
+        Path path = Paths.get("files/" + name);
+        try {
+            file = Files.readAllLines(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
+    public static void forwardingUDP(InetAddress dest, String msg) {
+
+        try {
+            byte[] sendData = new byte[1024];
+            DatagramSocket clientUDP = new DatagramSocket();
+            sendData = msg.getBytes();
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, dest, 9999);
+            clientUDP.send(sendPacket);
+            clientUDP.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static int cmp (String a){
+        int i=0;
+        while(i < a.length()){
+            char l = a.charAt(i);
+            int ascci = (int) l;
+            if(l == 0){
+                break;
+            }
+            else{
+                i++;
+            }
+        }
+        return i;
+    }
+
+
+}
